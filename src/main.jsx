cfaefc854dc2639923acac2d787fda7a12072c9b
ys <ysdeveloper01@gmail.com>
2023-12-10 18:30:01 +0800

    new router-dom data api

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+import Root, { loader as rootloader, action as rootAction } from "./routes/root"
+import ErrorPage from "./error-page"
+import Contact, { loader as contactLoader } from "./routes/contact"
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootloader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
